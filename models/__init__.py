4f7dbde7afa61577e88b275c46c61b58727e6274
sqLisss25 <jonquil.quern1n@icloud.com>
2026-02-25 18:14:29 +0300

    Create models/__init__.py

---
+++ models/__init__.py
@@ -0,0 +1,17 @@
+from .user import User
+from .song import Song
+from .album import Album
+from .artist import Artist
+from .genre import Genre
+from .playlist import Playlist
+from .library import Library
+
+__all__ = [
+    'User',
+    'Song',
+    'Album',
+    'Artist',
+    'Genre',
+    'Playlist',
+    'Library'
+]
